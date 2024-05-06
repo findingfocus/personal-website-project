f6907a282a9a479c26fbf12cfda2f28d316d1bfa
Paul Thompson <pthompson27@cnm.edu>
2024-05-05 21:33:49 -0600

    starting to add header with name and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['index.html', './src/**/*.{js,css}',  "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'sandias': "linear-gradient(rgba(0, 15, 40, 1), rgba(0, 0, 10, 0.7)), url('sandias.png')"
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
