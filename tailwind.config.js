a2e5eae94df41dfb92a80f6cf5ebbc285c6d8039
Paul Thompson <pthompson27@cnm.edu>
2024-05-13 14:47:04 -0600

    added button configuration and rounded border for headshot

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['index.html', './src/**/*.{js,css}',  "./node_modules/flowbite/**/*.js"],
   theme: {
+    fontFamily: {
+      mono: ['Menlo', 'monospace']
+    },
     extend: {
       backgroundImage: {
         'sandias': "linear-gradient(rgba(0, 15, 40, 1), rgba(0, 0, 10, 0.7)), url('sandias.png')"
